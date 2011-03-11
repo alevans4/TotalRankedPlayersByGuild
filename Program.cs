6f2e6c7254b81b4ae437c9aa45d4df749fe60792
alevans4 <alevans4@gmail.com>
2011-03-11 18:44:17 -0500

    Signed-off-by: alevans4 <alevans4@gmail.com>

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using WatiN.Core;
 using HtmlAgilityPack;
+using System.Collections;
 
 
 namespace TotalRankedPlayersByGuild
@@ -19,17 +20,31 @@ namespace TotalRankedPlayersByGuild
             HtmlDocument doc;
             HtmlNodeCollection players;
             int pagecount = 1;
-            string url = @"http://www.worldoflogs.com/guilds/24254/rankings/players/?page={0}";
-
+            string url = @"http://www.worldoflogs.com/guilds/1240/rankings/players/?page={0}";
+            List<string> encounters = new List<string>() { "Magmaw", "Conclave of Wind", "Chimaeron", "Cho&#39;gall", "Maloriak", "Argaloth", "Valiona &amp; Theralion", "Omnotron Defense System", "Halfus Wyrmbreaker", "Nefarian", "Atramedes", "Al'Akir", "Twilight Ascendant Council" };
             do
             {
                 doc = htmlWeb.Load(String.Format(url, pagecount));
-                players = doc.DocumentNode.SelectNodes("//tr[@class='even' or @class='odd']/td[2]/a");
+                players = doc.DocumentNode.SelectNodes("//tr[@class='even' or @class='odd']");
                 if (players != null)
                 {
                     foreach (var player in players)
                     {
-                        AddPlayer(player.InnerText);
+                        //check to see if it is a current fight
+                        foreach (var encounter in encounters)
+                        {
+                            if (player.InnerText.Contains(encounter))
+                            {
+                                AddPlayer(player.SelectSingleNode("td[2]/a").InnerText);
+
+                                List<string> listing = new List<string>();
+                                listing.Add(player.SelectSingleNode("td[2]/a").InnerText.PadRight(20));
+                                listing.Add(player.SelectSingleNode("td[6]").InnerText.PadRight(30));
+                                listing.Add(player.SelectSingleNode("td[8]").InnerText);
+                                listing.Add(player.SelectSingleNode("td[1]/a/span").InnerText);
+                                Console.WriteLine(string.Join("\t", listing.ToArray()));
+                            }
+                        }
                     }
                 }
                 pagecount++;
@@ -39,7 +54,7 @@ namespace TotalRankedPlayersByGuild
             var sortedList = (from entry in rankCount orderby entry.Value descending select entry);
             foreach (var player in sortedList)
             {
-                Console.WriteLine(player.Key + "\t" + player.Value);
+                Console.WriteLine(player.Key.PadRight(12) + " " + player.Value);
             }
             Console.ReadLine();
 
